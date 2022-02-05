95418fda9f1a9672ec307bd11e9380852e55734d
FervKa <ferbohi@outlook.com>
2022-02-05 00:27:38 -0500

    Se crea la clase Person con sus getters y setters, y los atributos de esta clase

---
+++ Person.java
@@ -0,0 +1,51 @@
+public class Person {
+    public string name;
+    public String lastName1;
+    public String lastName2;
+    public Date dateBirth;
+    public float heigt;
+
+
+
+
+
+    public string getName() {
+        return name;
+    }
+
+    public void setName(string name) {
+        this.name = name;
+    }
+
+    public String getLastName1() {
+        return lastName1;
+    }
+
+    public void setLastName1(String lastName1) {
+        this.lastName1 = lastName1;
+    }
+
+    public String getLastName2() {
+        return lastName2;
+    }
+
+    public void setLastName2(String lastName2) {
+        this.lastName2 = lastName2;
+    }
+
+    public Date getDateBirth() {
+        return dateBirth;
+    }
+
+    public void setDateBirth(Date dateBirth) {
+        this.dateBirth = dateBirth;
+    }
+
+    public float getHeigt() {
+        return heigt;
+    }
+
+    public void setHeigt(float heigt) {
+        this.heigt = heigt;
+    }
+}
