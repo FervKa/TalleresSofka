5e0c552b0da69dfba1a22af58fd85cd7a90b748b
FervKa <ferbohi@outlook.com>
2022-02-05 20:33:49 -0500

    Terminación de la clase Fruit

--- Main.java
+++ Main.java
@@ -2,11 +2,9 @@
 
 public class Main {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
-        Fruit fruit1 = new Fruit();
 
-        fruit1.setColors({"blue"});
 
     }
 }
